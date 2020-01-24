091736fde588c94b904b4d4d4894e926afa2872c
lenik terenin <lenik.terenin@se4.space>
2020-01-24 14:20:02 +0900

    run_dso.sh added

---
+++ run_dso.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+~/dso/build/bin/dso_dataset \
+files=~/Downloads/vision.in.tum.de/sequence_$1/images.zip \
+calib=~/Downloads/vision.in.tum.de/sequence_$1/camera.txt \
+gamma=~/Downloads/vision.in.tum.de/sequence_$1/pcalib.txt \
+vignette=~/Downloads/vision.in.tum.de/sequence_$1/vignette.png \
+preset=0 \
+mode=0
